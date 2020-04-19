759643da60f7264290fe42149fbc7cc8b560ce0a
Ivor Ćelić <ivorcelic@gmail.com>
2020-04-19 22:02:53 +0200

    neuspješno

--- mamineslastice.tenja.hr/konfiguracija.php
+++ mamineslastice.tenja.hr/konfiguracija.php
@@ -2,25 +2,25 @@
 
 return [
     'nazivAPP' => 'Mamine Slastice',
-    
-    'url' => 'http://mamineslastice.tenja.hr/',
     /*
-    'url' => 'http://ms.tenja.hr/',
+    'url' => 'http://mamineslastice.tenja.hr/',
     */
-    'dev' => true,
+    'url' => 'http://ms.tenja.hr/',
     
+    'dev' => true,
+    /*
     'db' =>[
         'server' => 'localhost',
         'baza' => 'KnjigaRecepata',
         'korisnik' => 'edunova',
         'lozinka' => 'edunova'
     ]
-    /*
+    */
     'db' =>[
         'server' => 'localhost',
         'baza' => 'tenjahr_knjigarecepata',
         'korisnik' => 'tenjahr_ivor',
         'lozinka' => 'ObG^8IJY(W!n'
     ]
-    */
+    
 ];
